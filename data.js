2e321309d4ed8e7849753d113eb63cc763273791
Siddhant Patil <111656737+1543siddhant@users.noreply.github.com>
2024-06-10 21:54:19 +0530

    Update data.js

--- data.js
+++ data.js
@@ -337,6 +337,20 @@ const sampleListings = [
     location: "Serengeti National Park",
     country: "Tanzania",
   },
+   
+  {
+    title: "Beachfront Paradise",
+    description:
+      "Step out of your door onto the sandy beach. This beachfront condo offers the ultimate relaxation.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdGVsc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    },
+    price: 2000,
+    location: "Cancun",
+    country: "Mexico",
+  },
+
 ];
 
 module.exports = { data: sampleListings };
